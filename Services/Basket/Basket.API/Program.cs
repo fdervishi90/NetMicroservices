f72d6546fe833f0d01feb17d3026d5920c1898d5
Flamur Dervishi <fdervishi@ikanbi.com>
2023-09-18 23:16:11 +0200

    Basket.API

---
+++ Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,46 @@
+
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+
+namespace Basket.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddStackExchangeRedisCache(opts =>
+            {
+                opts.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
+
+            builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
